5c612cc0bb2c851b9aac84055f1be969d08a4f4b
Miteos1986 <mdmitriy1986@gmail.com>
2024-03-13 17:41:30 +0100

    task 4

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const elements = event.target.elements;
+
+    if (elements.email.value.trim() === "" || elements.password.value.trim() === "") {return alert('All form fields must be filled in')}
+
+    const info = {
+        email: elements.email.value.trim(),
+        password: elements.password.value.trim()
+    }
+    console.log (info)
+
+    event.target.reset();
+}
