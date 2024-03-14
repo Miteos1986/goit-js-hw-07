5f03173cb8ff58b4104ddf4d9fa90049d3acab69
Miteos1986 <mdmitriy1986@gmail.com>
2024-03-14 15:28:21 +0100

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,4 +1,43 @@
 // 2:29
+const inputEl = document.querySelector("input")
+const btnCreate = document.querySelector("data-create")
+const btnDestroy = document.querySelector("data-destroy")
+const box = document.querySelector("#boxes")
+
+let sizes = 30; 
+
+
+
+btnCreate.addEventListener("click", createMarcup);
+
+ function createMarcup(){
+if  (amount < 1 || amount > 100){
+  return;
+  }
+createBoxes(amount);
+}
+btnDestroy.addEventListener("click", destroyMurcap);
+function destroyMurcap(){
+  box.innerHTML = "";
+  input.value = 0;
+  sizes = 30;
+}
+
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++){
+const myBox = document.createElement("div")
+myBox.style.width = `${sizes}px`;
+myBox.style.height = `${sizes}px`
+myBox.style.backgroundColor = getRandomHexColor();
+box.append(myBox);
+sizes +=10;
+
+  }
+}
+
+
+
 
 
 function getRandomHexColor() {
